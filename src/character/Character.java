97509618d06fcdfbfb845158065dc13f6bfb0e36
Sachin Pande <sazap10@gmail.com>
2013-03-27 09:59:05 +0000

    adding attributes for characters

--- src/character/Character.java
+++ src/character/Character.java
@@ -7,8 +7,21 @@ import javafx.scene.image.ImageView;
 
 /**
  * @author Sachin
- *
+ * Character class acts as the base for all characters in the game
+ * Contains attributes present in all characters
  */
 public class Character extends ImageView{
+	String name;
+	String sex;
+	/* 
+	 * Attributes
+	 */
+	int HP;
+	int MP;
+	int AttackRating;
+	int DefenseRating;
+	/*
+	 * End Attributes
+	 */
 	
 }
