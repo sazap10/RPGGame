79f94adcff6bb60791fcb5ff5999cb8cefe0151a
Sachin Pande <sazap10@gmail.com>
2013-03-29 16:45:25 +0000

    added main menu images

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 RPG-Game
 ========
+
+My first attempt at a RPG Game, written in JavaFX
---
+++ src/screens/MainMenu.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package screens;
+
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+
+/**
+ * @author Sachin
+ *
+ */
+public class MainMenu extends Scene{
+
+	/**
+	 * @param arg0
+	 */
+	public MainMenu(Parent arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+		
+	}
+
+}
